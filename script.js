c68d5ca83d2cba0b67557fa6cd7f070fb6bd1bcd
George Erimia <l3ttuceftw@gmail.com>
2025-03-10 20:24:32 +0200

    add mock sign in button functionality

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("script is active!");
+
+// Mock the sign in button for testing purposes
+let signInButton = document.querySelector("#btn-sign-in");
+signInButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  window.location = "./home-page.html";
+});
