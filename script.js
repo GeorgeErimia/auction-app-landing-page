7eca8dc491ad6c653e2d77f135e998edf2116486
George Erimia <l3ttuceftw@gmail.com>
2025-03-11 18:00:16 +0200

    added styles to home page and category list

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ console.log("script is active!");
 
 // Mock the sign in button for testing purposes
 let signInButton = document.querySelector("#btn-sign-in");
-signInButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  window.location = "./home-page.html";
-});
+if (signInButton !== null) {
+  signInButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.location = "./home-page.html";
+  });
+}
+
+// Mock the register button as well
+let registerButton = document.querySelector("#btn-create-account");
+if (registerButton !== null) {
+  registerButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.location = "./home-page.html";
+  });
+}
+
+// Add hover effect to category lists
+let categories = document.querySelectorAll(".category");
